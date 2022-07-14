e1f4dd04e9900c0978d6bf649bcd0e110749d09d
jadersonmarc <jadersonmarc@gmail.com>
2022-07-13 23:36:40 -0300

    add config test and test user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { '$2y$10$bLrDbU7tWDVLjhtxtOhkjO34MjTXGDO78FBo' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  it { expect(user).to respond_to(:email) }
+  it { expect(user).to respond_to(:password_confirmation) }
+  it { expect(user).to respond_to(:password) }
+
+  context 'when name is blank' do
+    before { user.name = " " }
+    it { expect(user).to validate_presence_of(:name)}
+  end
+
+  context 'when email is blank' do
+    before { user.email = " " }
+    it { expect(user).to validate_presence_of(:email)}
+  end
+
+  context 'when password is blank' do
+    before { user.password = " " }
+    it { expect(user).to validate_presence_of(:password)}
+  end
+
 end
