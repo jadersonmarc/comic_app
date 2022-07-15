b52506caeb2f3ce5ba79941b38edca4e57be3ac2
jadersonmarc <jadersonmarc@gmail.com>
2022-07-15 16:30:33 -0300

    add keep

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Apis", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    skip "is skipped" do
+    end
   end
 end
