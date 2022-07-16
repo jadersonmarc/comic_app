6451f06ec42ad7a61df2f093ef75465d7e1987e7
jadersonmarc <jadersonmarc@gmail.com>
2022-07-15 22:05:47 -0300

    add test generate token

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     password { '$2y$10$bLrDbU7tWDVLjhtxtOhkjO34MjTXGDO78FBo' }
   end
--- spec/models/json_web_token_spec.rb
+++ spec/models/json_web_token_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe JsonWebToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:payload) { { sub: user.id, exp: 30.minutes.from_now.to_i } }
+
+  context 'authentication by token jwt' do
+    it 'generate a token' do
+      jwt = JsonWebToken.encode(payload)
+      decode = JsonWebToken.decode(jwt)
+      expect(user.id).to eq(decode['sub'])
+    end
+  end
+
+  context 'expiration token' do
+    before do
+      payload['exp'] = 0
+    end
+    it 'expired token' do
+      jwt = JsonWebToken.encode(payload)
+      expect { JsonWebToken.decode(jwt) }.to raise_error(JWT::ExpiredSignature)
+    end
+  end
+
 end
